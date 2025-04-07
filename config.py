68de6bd1ae76b9149cfb0c6d23c9cff9afe32fe2
Tosu <167279662+about-tosu@users.noreply.github.com>
2025-04-07 09:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,18 +78,18 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/97669c286e18c2eddc72d.jpg"
+    "START_IMG_URL", "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/97669c286e18c2eddc72d.jpg"
+    "PING_IMG_URL", "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
-STATS_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
-STREAM_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
+STATS_IMG_URL = "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/cf0cb9c6645dab914d406-5e1d589294e2cf400c.jpg"
+STREAM_IMG_URL = "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/604f8104e1f86cdb2d0e5.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/97669c286e18c2eddc72d.jpg"
