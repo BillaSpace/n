93d5018c63275d0cc6ea354d966e9c06cae20ea4
BillaSpace <SmokenX1@gmail.com>
2025-07-09 10:14:20 +0530

    Update __init__.py

--- AnonXMusic/plugins/__init__.py
+++ AnonXMusic/plugins/__init__.py
@@ -1,13 +1,14 @@
 import glob
+import os
 from os.path import dirname, isfile
 
 
 def __list_all_modules():
     work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+    mod_paths = glob.glob(os.path.join(work_dir, "*", "*.py"))
 
     all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        os.path.relpath(f, work_dir).replace(os.sep, ".")[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
