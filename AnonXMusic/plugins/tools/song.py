0b708a4a326e8c635a4cd26f2797cf513978550a
BillaSpace <SmokenX1@gmail.com>
2025-07-12 17:38:32 +0530

    Update and rename songs.py to song.py

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import yt_dlp
 import time
+import re
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
